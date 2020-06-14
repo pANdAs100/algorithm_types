42a57a7d9211af8db27d5159960986f4d2ab3789
Laura Hobbs <believeinthefuture@outlook.com>
2020-06-13 23:19:37 -0700

    greedy algo solves coin problem, displays expected time complexity shortcomming and is optimised.

--- algorithms.js
+++ algorithms.js
@@ -36,34 +36,41 @@ document.getElementById("test-2").innerHTML
 //Objective total: c18
 //Takes the highest value in the array and checks if it goes over the targeted sum
 //if it does not then this ammount is accumulated. If it does then that element is removed 
-function greedyFunc(a,b,c,d){
+function greedyFunc(trgt, ...coins){
     console.log("--greedy function--");
     var numberOfSteps = 0;
-    const TARGETSUM = 15
-    var coins = [a,b,c,d];
+    const TARGETSUM = trgt;
+    // var coins = [a,b,c,d]; The prototype of the function can also have use rest to make the number of arguments variable
     var sum = Math.max(...coins);//sum is our accumulator
     var max = Math.max(...coins); //just makes it so we dont have to keep writing Math.max(...coins)
     // console.log((sum + Math.max(...coins)) < TARGETSUM);
     while(coins.length > 0){
         if((sum + max) <= TARGETSUM){
-            console.log("TRUE: (sum + max) <= TARGETSUM")
-            console.log("Therefor " + max + " is added to " + sum)
+            // console.log("TRUE: (sum + max) <= TARGETSUM")
+            // console.log("Therefor " + max + " is added to " + sum)
             sum += max;
-            console.log("new sum is " + sum);
-            numberOfSteps++;
+            // console.log("new sum is " + sum);
         } else {
-            console.log("SUM + MAX IS GREATER THAN THE TARGET SUM");
-            console.log(max + " is removed from " + coins);
+            // console.log("SUM + MAX IS GREATER THAN THE TARGET SUM");
+            // console.log(max + " is removed from " + coins);
             coins = coins.filter((e) => {return e != max});
             max = Math.max(...coins);
-            numberOfSteps++;
         }
+        numberOfSteps++;
     }
     return numberOfSteps;
 }
 
 //by changing the TARGETSUM from 18 to 15 we can clearly see that the greedyFunction is not optimum for most use cases
-const ar2 = [1,1,7,10];
+const ar2 = [1,7,10];
+const testNumOne = 18;
+const testNumTwo = 15;
 
-document.getElementById("algorithm-1").innerHTML += greedyFunc(...ar1);
-document.getElementById("algorithm-1-2").innerHTML += greedyFunc(...ar2);
+document.getElementById("algorithm-1").innerHTML += greedyFunc(testNumTwo,...ar1);
+document.getElementById("algorithm-1-2").innerHTML += greedyFunc(testNumTwo,...ar2);
+
+//BINARY SEARCH
+
+//SOURCES:
+// https://www.tutorialspoint.com/data_structures_algorithms/greedy_algorithms.htm
+// https://www.tutorialspoint.com/data_structures_algorithms/binary_search_algorithm.htm
